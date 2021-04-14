f6a4d246737892ac3382e7a8673598a302c285e9
youngkwonnoh <yknoh.sol@gmail.com>
2021-04-14 18:09:11 +0900

    select query

--- hr.sql
+++ hr.sql
@@ -6,3 +6,34 @@ select * from employees;
 
 -- employees 테이블의 first_name, last_name, job_id 만 조회
 SELECT first_name, last_name, job_id FROM employees;
+
+-- employee
+-- 사원번호가 176인 사람의 LAST_NAME 과 부서 번호를 조회한다.
+SELECT last_name, department_id FROM employees WHERE employee_id = 176;
+
+-- SALARY가 12000 이상되는 직원들의  LAST_NAME 과 SALARY를 조회한다.
+SELECT last_name, salary FROM employees WHERE salary > 12000;
+
+-- SALARY가 5000에서 12000의 범위 이외의 사람들의 LAST_NAME 과 SALARY를 조회한다.
+SELECT last_name, salary FROM employees WHERE salary < 5000 AND salary > 12000;
+
+
+-- 20번, 50번 부서에서 근무하는 모든 사원들의 LAST_NAME 및 부서번호를 알파벳 순으로 조회
+SELECT last_name, department_id FROM employees WHERE department_id = 20 OR department_id = 50 ORDER BY last_name ASC;
+SELECT last_name, department_id FROM employees WHERE department_id IN(20, 50) ORDER BY last_name ASC;
+
+-- COMMISSION_PCT을 버는 모든 사원들의 LAST_NAME, SALARY, COMMISSION_PCT를 조회
+-- SALARY, COMMISSION_PCT 내림차순으로 정렬
+SELECT LAST_NAME, SALARY, COMMISSION_PCT FROM employees WHERE commission_pct > 0 ORDER BY SALARY DESC, COMMISSION_PCT DESC;
+
+-- SALARY이 2500, 3500, 7000이 아니며 직업이 SA_REP 혹은 ST_CLERK인 사원 조회
+SELECT * FROM employees WHERE SALARY NOT IN(2500, 3500, 7000) AND JOB_ID IN('SA_REP', 'ST_CLERK');
+
+-- 고용일자가 2008-02-20 ~ 2008-05-01 사이에 고용된 사원들의 LAST_NAME, EMPLOYEE_ID, HIRE_DATE
+-- 를 조회한 후 고용일자 내림차순으로 정렬(날짜 데이터는 '' 사이에 넣어서 처리)
+SELECT LAST_NAME, EMPLOYEE_ID, HIRE_DATE FROM employees WHERE HIRE_DATE >= '2008-02-20' AND HIRE_DATE <= '2008-05-01' ORDER BY HIRE_DATE DESC; 
+
+-- 2004년도에 고용된 사람들의 LAST_NAME, HIRE_DATE를 조회한 후 입사일을 기준으로 오름차순 정렬
+SELECT last_name, HIRE_DATE FROM employees WHERE HIRE_DATE >= '2004-01-01' AND HIRE_DATE <= '2004-12-31' ORDER BY HIRE_DATE ASC;
+
+
--- scott.sql
+++ scott.sql
@@ -46,4 +46,81 @@ SELECT EMPNO, ENAME, JOB FROM emp ORDER BY EMPNO ASC;
 -- 부서번호의 오름차순과 급여의 내림차순 정렬하기
 select * from emp Order BY deptno ASC, sal DESC;
 
--- 
+-- 조회할 테이블은 EMP 테이블이며 모든 열을 출력
+-- 출력되는 열의 별칭을 다음과 같이 지정
+-- EMPNO : EMPLOYEE_NO
+-- ENAME : EMPLOYEE_NAME
+-- MGR : EMPLOYEE_NAME
+-- SAL : SALARY
+-- COMM : COMMISSION
+-- DEPTNO : DEPARTMENT_NO
+-- 부서 번호를 기준으로 내림차순으로 정렬
+-- 부서 번호가 같다면 사원 이름을 기준으로 오름차순 정렬
+
+
+SELECT 
+EMPNO AS EMPLOYEE_NO, ENAME AS EMPLOYEE_NAME, 
+MGR AS EMPLOYEE_NAME, SAL AS SALARY, COMM AS COMMISSION, 
+DEPTNO AS DEPARTMENT_NO  
+FROM EMP ORDER BY DEPTNO DESC, ENAME ASC;
+
+-- WHERE : 조건에 맞춰서 조회
+
+-- 부서번호가 30번인 사원 조회
+SELECT * FROM emp WHERE deptno = 30;
+
+-- 사원번호가 7782인 사원 정보 조회
+SELECT * FROM emp WHERE empno = 7782;
+
+-- 부서번호가 30이고 사원 직책이 SALESMAN인 사원 조회
+SELECT * FROM emp WHERE DEPTNO = 30 AND job = 'SALESMAN';
+
+-- 부서번호가 30이거나 사원직책이 CLERK인 사원 조회
+SELECT * FROM emp WHERE DEPTNO = 30 AND job = 'CLERKL';
+
+
+-- 산술연산자 : * / + -
+
+-- SAL*12의 값이 36000인 사원 조회
+SELECT * FROM EMP WHERE SAL * 12 = 36000;
+
+-- SAL의 값이 3000 이상인 사원 조회
+SELECT * FROM EMP WHERE SAL >= 3000;
+
+-- 급여가 2500이고, 직업이 ANALYST인 사원 조회
+SELECT * FROM EMP WHERE SAL >= 2500 AND JOB = 'ANALYST';
+
+-- 사원 이름의 첫문자가 F와 같거나 뒤쪽인 것만 조회(F, G, H, I ~~)
+SELECT * FROM EMP WHERE ENAME >= 'F';
+
+-- 직무가 MANAGER, SALESMAN, CLERK인 사원 조회
+SELECT * FROM EMP WHERE JOB = 'MANAGER' OR JOB = 'SALESMAN' OR JOB = 'CLERK';
+
+-- 등가 비교 연산자( = (동등), !=, <>, ^= )
+-- SALㅇ; 3000이 아닌 사원정보 조회
+SELECT * FROM EMP WHERE SAL != 3000;
+SELECT * FROM EMP WHERE SAL ^= 3000;
+SELECT * FROM EMP WHERE SAL <> 3000;
+
+-- 논리 부정 연산자 : NOT
+SELECT * FROM EMP WHERE NOT SAL = 3000;
+
+-- IN은 =과 같은 개념이다.
+SELECT * FROM EMP WHERE JOB = 'MANAGER' OR JOB = 'SALESMAN' OR JOB = 'CLERK';
+SELECT * FROM EMP WHERE JOB IN('MANAGER', 'SALESMAN', 'CLERK');
+
+-- NOT IN
+SELECT * FROM EMP WHERE JOB != 'MANAGER' AND JOB ^= 'SALESMAN' AND JOB <> 'CLERK';
+SELECT * FROM EMP WHERE JOB NOT IN('MANAGER', 'SALESMAN', 'CLERK');
+
+-- 부서번호가 10, 20 사원 조회(IN)
+SELECT * FROM EMP WHERE DEPTNO = 10 OR DEPTNO = 20;
+SELECT * FROM EMP WHERE DEPTNO IN(10, 20);
+
+-- BETWEEN A AND B : 특정 범위의 데이터를 조회
+SELECT * FROM EMP WHERE SAL >= 2000 AND SAL <= 3000;
+SELECT * FROM EMP WHERE SAL BETWEEN 2000 AND 3000;
+
+-- SAL 2000이상 ~ 3000이하가 아닌 사원 조회
+SELECT * FROM EMP WHERE SAL NOT BETWEEN 2000 AND 3000;
+
